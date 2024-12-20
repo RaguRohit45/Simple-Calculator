a5922fec759f7ec8ecb9c08afe4304ff070450e7
RaguRohit45 <717823y141@kce.ac.in>
2024-12-20 20:34:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function calc(val){
+    document.getElementById("inp").value+=val;
+}
+function tot(){
+    document.getElementById("inp").value=eval(document.getElementById("inp").value);
+}
+function cle(){
+    document.getElementById("inp").value="";
+}
